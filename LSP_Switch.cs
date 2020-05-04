0371771fa29d5595f67de7ab5a98b13e8f8bba29
SwetaMitt <64412885+SwetaMitt@users.noreply.github.com>
2020-05-04 10:01:11 +0530

    Add files via upload

---
+++ LSP_Switch.cs
@@ -0,0 +1,176 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+
+// Liskov Substitution Principle using down casting and switch
+namespace ConsoleApplication1
+{
+    public abstract class DocumentPart
+    {
+        public abstract void Paint();
+
+        public abstract void Save();
+
+    }
+
+
+    public class Paragraph : DocumentPart
+    {
+        public override void Paint()
+        {
+           Console.WriteLine("");
+        }
+
+        public override void Save()
+        {
+           Console.WriteLine("");
+        }
+
+    }
+
+    public class HyperLink : DocumentPart
+    {
+        public override void Paint()
+        {
+           Console.WriteLine("");
+        }
+
+        public override void Save()
+        {
+           Console.WriteLine("");
+        }
+    }
+
+
+    public class Header : DocumentPart
+    {
+        public override void Paint()
+        {
+           Console.WriteLine("");
+        }
+
+        public override void Save()
+        {
+           Console.WriteLine("");
+        }
+    }
+
+    public class Footer : DocumentPart
+    {
+        public override void Paint()
+        {
+            Console.WriteLine("");
+        }
+
+        public override void Save()
+        {
+            Console.WriteLine("");
+        }
+    }
+    public interface IDocumentConverter
+    {
+        void ConvertParagraph(Paragraph obj);
+		void ConvertHeader(Header obj);
+        void ConvertFooter(Footer obj);
+        void ConvertHyperLink(HyperLink obj);
+    }
+
+    public class PDFConverter : IDocumentConverter
+    {
+        public void ConvertParagraph(Paragraph obj)
+        {
+            Console.WriteLine("PDFConverter:Paragraph");
+        }
+
+        public void ConvertHeader(Header obj)
+        {
+            Console.WriteLine("PDFConverter:Header");
+        }
+
+        public void ConvertFooter(Footer obj)
+        {
+            Console.WriteLine("PDFConverter:Footer");
+        }
+
+        public void ConvertHyperLink(HyperLink obj)
+        {
+            Console.WriteLine("PDFConverter:HyperLink");
+        }
+    }
+
+    public class HTMLConverter : IDocumentConverter
+    {
+        public void ConvertParagraph(Paragraph obj)
+        {
+            Console.WriteLine("HTMLConverter:Paragraph");
+        }
+
+        public void ConvertHeader(Header obj)
+        {
+            Console.WriteLine("HTMLConverter:Header");
+        }
+
+        public void ConvertFooter(Footer obj)
+        {
+            Console.WriteLine("HTMLConverter:Footer");
+        }
+
+        public void ConvertHyperLink(HyperLink obj)
+        {
+            Console.WriteLine("HTMLConverter:HyperLink");
+        }
+    }
+
+    public class Document1
+    {
+        public List<DocumentPart> parts = new List<DocumentPart>();
+
+        public void Add(DocumentPart doc)
+        {
+            parts.Add(doc);
+
+        }
+
+        public void Convert(IDocumentConverter obj)
+        {
+            PDFConverter p = new PDFConverter();
+            foreach (var v in parts)
+            {
+                //p.ConvertParagraph(v);
+                Type type = v.GetType();
+                Console.WriteLine(type.ToString());
+                switch (type.Name)
+                {
+                    case "Paragraph":
+                        p.ConvertParagraph((Paragraph)v);
+                        break;
+                    case "Header":
+                        p.ConvertHeader((Header)v);
+                        break;
+                    case "Footer":
+                        p.ConvertFooter((Footer)v);
+                        break;
+                    case "HyperLink":
+                        p.ConvertHyperLink((HyperLink)v);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+    public class LSP_Switch
+    {
+        public static void Main()
+        {
+            Document1 obj = new Document1();
+            obj.Add(new Paragraph());
+            obj.Add(new Header());
+            obj.Add(new Footer());
+            obj.Add(new HyperLink());
+            obj.Convert(new PDFConverter());
+        }
+    }
+}
---
+++ OCP_Visitor.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+
+// This program solves OCP using Visitor
+namespace OCP_Visitor
+{
+    public abstract class DocumentPart
+    {
+        public abstract void Paint();
+
+        public abstract void Save();
+
+        public abstract void Convert(IDocumentConverter obj);
+
+    }
+
+
+    public class Paragraph : DocumentPart
+    {
+        public override void Paint()
+        {
+            Console.WriteLine("");
+        }
+
+        public override void Save()
+        {
+            Console.WriteLine("");
+        }
+
+        public override void Convert(IDocumentConverter converterObj)
+        {
+            converterObj.ConvertParagraph(this);
+        }
+
+    }
+
+    public class HyperLink : DocumentPart
+    {
+        public override void Paint()
+        {
+            Console.WriteLine("");
+        }
+
+        public override void Save()
+        {
+            Console.WriteLine("");
+        }
+
+        public override void Convert(IDocumentConverter converterObj)
+        {
+            converterObj.ConvertHyperLink(this);
+        }
+    }
+
+
+    public class Header : DocumentPart
+    {
+        public override void Paint()
+        {
+            Console.WriteLine("");
+        }
+
+        public override void Save()
+        {
+            Console.WriteLine("");
+        }
+
+        public override void Convert(IDocumentConverter converterObj)
+        {
+            converterObj.ConvertHeader(this);
+        }
+    }
+
+    public class Footer : DocumentPart
+    {
+        public override void Paint()
+        {
+            Console.WriteLine("");
+        }
+
+        public override void Save()
+        {
+            Console.WriteLine("");
+        }
+
+        public override void Convert(IDocumentConverter converterObj)
+        {
+            converterObj.ConvertFooter(this);
+        }
+    }
+    public interface IDocumentConverter
+    {
+        void ConvertParagraph(Paragraph obj);
+        void ConvertHeader(Header obj);
+        void ConvertFooter(Footer obj);
+        void ConvertHyperLink(HyperLink obj);
+    }
+
+    public class PDFConverter : IDocumentConverter
+    {
+        public void ConvertParagraph(Paragraph obj)
+        {
+            Console.WriteLine("PDFConverter:Paragraph");
+        }
+
+        public void ConvertHeader(Header obj)
+        {
+            Console.WriteLine("PDFConverter:Header");
+        }
+
+        public void ConvertFooter(Footer obj)
+        {
+            Console.WriteLine("PDFConverter:Footer");
+        }
+
+        public void ConvertHyperLink(HyperLink obj)
+        {
+            Console.WriteLine("PDFConverter:HyperLink");
+        }
+    }
+
+    public class HTMLConverter : IDocumentConverter
+    {
+        public void ConvertParagraph(Paragraph obj)
+        {
+            Console.WriteLine("HTMLConverter:Paragraph");
+        }
+
+        public void ConvertHeader(Header obj)
+        {
+            Console.WriteLine("HTMLConverter:Header");
+        }
+
+        public void ConvertFooter(Footer obj)
+        {
+            Console.WriteLine("HTMLConverter:Footer");
+        }
+
+        public void ConvertHyperLink(HyperLink obj)
+        {
+            Console.WriteLine("HTMLConverter:HyperLink");
+        }
+    }
+
+    public class Document1
+    {
+        public List<DocumentPart> parts = new List<DocumentPart>();
+
+        public void Add(DocumentPart doc)
+        {
+            parts.Add(doc);
+
+        }
+
+        public void Convert(IDocumentConverter obj)
+        {
+            foreach (var v in parts)
+            {
+                v.Convert(obj);
+            }
+        }
+    }
+
+    public class OCP_Visitor
+    {
+        public static void Main()
+        {
+            Document1 obj = new Document1();
+            obj.Add(new Paragraph());
+            obj.Add(new Header());
+            obj.Add(new Footer());
+            obj.Add(new HyperLink());
+            obj.Convert(new PDFConverter());
+            obj.Convert(new HTMLConverter());
+        }
+    }
+}
