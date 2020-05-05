212da979af769c0b6d91f99e36abdf07010aa05b
SwetaMitt <64412885+SwetaMitt@users.noreply.github.com>
2020-05-05 08:52:41 +0530

    Add files via upload

---
+++ ChainOfResponsibility.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChainOfResponsibility
+{
+    class ChainOfResponsibilityProg
+    {
+        //Abstract class
+        abstract class Component
+        {
+            protected Component successor;
+            public void SetSuccessor(Component successor)
+            {
+                this.successor = successor;
+            }
+            public abstract void HandleRequest();
+        }
+
+        class ConcreteComponent1 : Component
+        {
+            public override void HandleRequest()
+            {
+                Console.WriteLine("{0} handled request",
+                    this.GetType().Name);
+                successor?.HandleRequest();
+            }
+        }
+
+        class ConcreteComponent2 : Component
+        {
+            public override void HandleRequest()
+            {
+                Console.WriteLine("{0} handled request",
+                    this.GetType().Name);
+
+                successor?.HandleRequest();
+            }
+        }
+
+        class ConcreteComponent3 : Component
+        {
+            public override void HandleRequest()
+            {
+                Console.WriteLine("{0} handled request",
+                    this.GetType().Name);
+                successor?.HandleRequest();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // Setup Chain of Responsibility
+
+            Component h1 = new ConcreteComponent1();
+            Component h2 = new ConcreteComponent2();
+            Component h3 = new ConcreteComponent3();
+            h1.SetSuccessor(h2);
+            h2.SetSuccessor(h3);
+
+            h1.HandleRequest();
+
+        }
+    }
+}
